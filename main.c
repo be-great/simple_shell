8abb8edac6c4f6669a9db1ea6d195dd4a580276b
be-great <begreat5333@gmail.com>
2023-12-01 15:20:59 +0200

    add main file structure

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int main(int ac, char **argv)
+{
+    
+    return (0);
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#endif
